534df2024c742b2556e5de11da5f8808b3452de9
Wee Sing Yap <yap_wee_sing@tech.gov.sg>
2018-04-24 17:18:14 +0800

    Changed redis URI for RPaaS configuration.

--- config/rpaas.js
+++ config/rpaas.js
@@ -5,7 +5,7 @@ var config = {
     port: 3000,
 
     redis: {
-        uri: 'redis://fightclubredis.tsjtwu.ng.0001.apse1.cache.amazonaws.com:6379',
+        uri: 'redis://nectar-app-redis.tsjtwu.ng.0001.apse1.cache.amazonaws.com:6379',
         dbIndex: 0
     },
     game:{
